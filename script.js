0a9c10b29f3b7ec185c8870b86837a296ad04659
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-07-26 17:21:26 -0400

    added wyoming flag

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let timeline = new TimelineMax();
 timeline
     .to('.infoContainer', .25, { y: -50 })
     .to('.foreground', 3, { y: -50 })
-    .to('.background', 3, { y: 130 }, "-=3")
+    .to('.background', 3, { y: 200 }, "-=3")
     .to('.headText', 2.5, { y: -250 }, "-=3")
     .fromTo('.infoImage', { opacity: 0 }, { opacity: 1, duration: 3 })
     .fromTo('.textDesc', { opacity: 0 }, { opacity: 1, duration: 3 });
