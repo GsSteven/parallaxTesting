e49feda0d96f5c9853d69e779eb04e37e571c504
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-07-26 16:13:19 -0400

    new page image

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-    .to('.foreground', 3, { y: -75 })
-    .to('.background', 3, { y: 150 }, "-=3")
+    .to('.foreground', 3, { y: -35 })
+    .to('.background', 3, { y: 130 }, "-=3")
     .to('.headText', 5, { y: -400 }, "-=3")
     .to('.infoContainer', 3, { top: "0%" }, "-=3")
     .fromTo('.infoImage', { opacity: 0 }, { opacity: 1, duration: 3 })
