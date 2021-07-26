7447eb05c2f0000d5e685b87ca92f956fc190c42
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-07-26 14:15:39 -0400

    smoother parallax animation and fading in subtext

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let timeline = new TimelineMax();
 
 timeline
     .to('.foreground', 3, { y: -75 })
-    .to('.background', 3, { y: 200 }, "-=3")
+    .to('.background', 3, { y: 150 }, "-=3")
     .to('.headText', 5, { y: -400 }, "-=3")
-    .to('.infoContainer', 3, { top: "0%" }, "-=3");
+    .to('.infoContainer', 3, { top: "0%" }, "-=3")
+    .fromTo('.infoImage', { opacity: 0 }, { opacity: 1, duration: 3 })
+    .fromTo('.textDesc', { opacity: 0 }, { opacity: 1, duration: 3 });
 
 
 let scene = new ScrollMagic.Scene({
